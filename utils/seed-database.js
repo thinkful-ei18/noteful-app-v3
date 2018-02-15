85978d6aa69af73c3a023e6632078dc3bc868d11
Chris Klanac <cklanac@users.noreply.github.com>
2018-02-15 13:52:28 -0600

    simplify seeddata

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -11,12 +11,8 @@ const seedFolders = require('../db/seed/folders');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
-  // .then(() => Note.insertMany(seedNotes))
-  .then(() => {
-    const noteInsertPromise = Note.insertMany(seedNotes);
-    const folderInsertPromise = Folder.insertMany(seedFolders);
-    return Promise.all([noteInsertPromise, folderInsertPromise]);
-  })
+  .then(() => Folder.insertMany(seedFolders))
+  .then(() => Note.insertMany(seedNotes))
   .then(() => Note.createIndexes())
   .then(() => mongoose.disconnect())
   .catch(err => {
